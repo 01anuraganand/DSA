4e2ff14875e5914698d4ab8cf1c17e7ce0d068cb
Anurag Anand <anuraganand7618@gmail.com>
2022-05-25 10:58:04 +0530

    Update permutation.cpp

--- Recursion/permutation.cpp
+++ Recursion/permutation.cpp
@@ -22,14 +22,35 @@ void permutation1(vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans, i
 	}
 }
 
+void permutation2(int index, vector<int> &arr, vector<vector<int>> &ans)
+{
+	if( index == arr.size())
+	{
+		ans.push_back(arr);
+		return;
+	}
+	
+	for(int i = index; i<arr.size(); i++)
+	{
+		swap(arr[index], arr[i]);
+		permutation2(index+1, arr, ans);
+		swap(arr[index], arr[i]);
+	}
+}
+
 vector<vector<int>> permutate(vector<int> &arr)
 {
 	vector<vector<int>> ans;
 	vector<int> ds;
-	int freq[arr.size()];
-	for(int i = 0; i<arr.size(); i++) freq[i] =0;
+	int freq[arr.size()] = {0};
 	permutation1(ds, arr, ans, freq);
-	
+	return ans;
+}
+
+vector<vector<int>> permutate2(vector<int> &arr)
+{
+	vector<vector<int>> ans;
+	permutation2(0, arr, ans);
 	return ans;
 }
 
@@ -39,6 +60,17 @@ int main()
 	vector<int> arr{1,2,3};
 	ans = permutate(arr);
 	for (auto vec1:ans)
+	{
+		cout<<endl;
+		for(auto it: vec1)
+			cout<<it<< " ";
+	}
+	
+	
+	cout<<endl;
+	vector<vector<int>> ans1;
+	ans1 = permutate2(arr);
+	for (auto vec1:ans1)
 	{
 		cout<<endl;
 		for(auto it: vec1)
